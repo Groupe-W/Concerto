49c2c52028bec2a121620d688f42248efbd9c740
Vincent <Cephalobyte@users.noreply.github.com>
2021-03-07 12:22:32 -0500

    Open Stage Control répond pu

--- Fichiers de projet/OSC/Custom Modules/customModule1.js
+++ Fichiers de projet/OSC/Custom Modules/customModule1.js
@@ -1,29 +1,52 @@
+let oscIn, oscOut;
+
 module.exports = {
+
+	init: function(){
+		oSCOut
+		oSCIn
+	},
 
 	oscInFilter:function(data){
 
-			var {address, args, host, port} = data
+		var {address, args, host, port} = data
 
-			if (address === '/fader_cool') {
+		if (address === '/fader_cool') {
 
-					args[0].value = args[0].value * 10
+			args[0].value = args[0].value * 10
 
-			}
+		} else if (address === '/ts_n_fader') {
 
-			return {address, args, host, port}
+			address = '/ts_n';
+			args[0].value = Math.round(args[0].value * 31 + 1);
+
+		}
+
+		return {address, args, host, port}
 
 	},
 
 	oscOutFilter:function(data){
 
-			var {address, args, host, port, clientId} = data
+		var {address, args, host, port, clientId} = data
 
-			if (address === '/fader_cool') {
+		if (address === '/fader_cool') {
 
-					args[0].value = args[0].value / 10
+			args[0].value = args[0].value / 10
 
-			}
+		} else if (address === '/ts_n+') {
 
-			return {address, args, host, port}
+			address = '/ts_n';
+
+		} else if (address === '/ts_n-') {
+
+			address = '/ts_n';
+			
+			console.log("send",settings.read("send"));
+			send(host, port, '/who_is_there');
+
+		}
+
+		return {address, args, host, port}
 	}
 }
